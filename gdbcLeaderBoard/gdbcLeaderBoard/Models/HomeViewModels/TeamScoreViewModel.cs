4f571b249cee94d40f449d36cb3ee47f8b84721d
Geert van der Cruijsen <geert@vdcruijsen.net>
2017-06-02 16:11:25 +0200

    added scores to homeview

---
+++ gdbcLeaderBoard/gdbcLeaderBoard/Models/HomeViewModels/TeamScoreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gdbcLeaderBoard.Models.HomeViewModels
+{
+    public class TeamScoreViewModel
+    {
+        public string Team { get; set; }
+        public int Score { get; set; }
+    }
+}
