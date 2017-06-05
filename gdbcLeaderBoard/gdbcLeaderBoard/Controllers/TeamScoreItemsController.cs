7d22b1fdf81a1bf731a490db37a0ba0b4fb7333f
peter groenewegen <pgroene@gmail.com>
2017-06-05 18:55:44 +0200

    fix authorizations

--- gdbcLeaderBoard/gdbcLeaderBoard/Controllers/TeamScoreItemsController.cs
+++ gdbcLeaderBoard/gdbcLeaderBoard/Controllers/TeamScoreItemsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using gdbcLeaderBoard.Data;
 using gdbcLeaderBoard.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace gdbcLeaderBoard.Controllers
 {
